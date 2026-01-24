7251b451db07177d7de9ccc7911fb58d46f21c73
Almurat Nurmerey <almuratnurmerei3@gmail.com>
2026-01-25 03:50:14 +0600

    Create strings.py

---
+++ python-basics/strings.py
@@ -0,0 +1,19 @@
+# ex 1: declare a string, and read the 4-th value
+x = "Afflary"
+print(x[3])
+
+# ex 2: finding the size (length) of a string
+x = "Bananananananana"
+print(len(x)) # <-- commas, spaces and all symbols are counted
+
+#  ex 3: read the 1-st value
+x = "Old Yharnam"
+print(x[0])
+
+# ex 4: if the word exist in string:
+x = "Bloodborne"
+print("born" in x) # bool type
+
+# ex 5: if the word is not exist, that is the reverse of the ex#4
+x = "Watch Dogs"
+print("Cats" not in x)
