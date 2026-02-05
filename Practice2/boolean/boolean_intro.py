f49cc3eb6b0e0e41dfb6dbcbd2df8168568c0468
Almurat Nurmerey <almuratnurmerei3@gmail.com>
2026-02-05 22:53:42 +0600

    Update boolean_intro.py

--- Practice2/boolean/boolean_intro.py
+++ Practice2/boolean/boolean_intro.py
@@ -1,1 +1,22 @@
-main()
+# ex 1: basic boolean values
+is_student = True
+is_teacher = False
+
+# ex 2: boolean values in integer
+number = 6
+empty_number = 0
+print(bool(number)) # True
+print(bool(empty_number)) # False
+
+# ex 3: boolean values in strings
+some_string = "Afflary Cake"
+empty_string = ""
+print(bool(some_string)) # True
+print(bool(empty_string)) # False
+
+# ex 4: boolean values in any containers
+
+my_list = ["new-york", "candles", 6]
+empty_list = []
+print(bool(my_list))
+print(bool(empty_list))
